cb1ed8e8101d9a3d2726bf512e7bc8f827da5bfd
RoyalBosS-Ayush <ayushmunot921@gmail.com>
2023-04-16 20:11:07 +0530

    code push auto

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+} from 'react-native';
+import CodePush from 'react-native-code-push';
+
+
+const App = () => {
+  return (
+    <SafeAreaView style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text style={{ color: "#faf" }}>HELLO ROYALBOSS</Text>
+
+    </SafeAreaView>
+  );
+};
+
+const CodePushOptions = { checkFrequence: CodePush.CheckFrequency.ON_APP_START }
+export default CodePush(CodePushOptions)(App);
